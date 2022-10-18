4ea5e99b78db10853dd6a642ab40d83f5048037c
MihailoDjordjevic <mdjordjevic3020rn@raf.rs>
2022-10-18 02:21:48 +0200

    Completed info dialog -added another student image and info

--- src/rs/raf/gerumap/controller/ActionInfo.java
+++ src/rs/raf/gerumap/controller/ActionInfo.java
@@ -17,25 +17,36 @@ public class ActionInfo implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         BasicDialog jDialog = new BasicDialog(MainFrame.getInstance(), "Info", true, 400, 400);
-        JPanel jPanel = new JPanel();
-        GridLayout gridLayout = new GridLayout(4, 2, 2, 1);
-        jPanel.setLayout(gridLayout);
 
-        JLabel jLabelIme = new JLabel("Ime:");
-        JLabel jLabelPrezime = new JLabel("Prezime:");
-        JLabel jLabelIndeks = new JLabel("Indeks:");
+        addDeveloperInfo(jDialog);
+
+        jDialog.setVisible(true);
+    }
+
+    private void addDeveloperInfo(BasicDialog jDialog){
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(4, 2, 2, 1));
+
+        JLabel jLabelIme = new JLabel("Ime: Lazar");
+        JLabel jLabelPrezime = new JLabel("Prezime: Bojanic");
+        JLabel jLabelIndeks = new JLabel("Indeks: 116/21/RN");
         JLabel jLabelSlika = new JLabel("Slika:");
 
-        JLabel jLabelImeContent = new JLabel("Lazar");
-        JLabel jLabelPrezimeContent = new JLabel("Bojanic");
-        JLabel jLabelIndeksContent = new JLabel("116/21/RN:");
-        JLabel jLabelSlikaContent = new JLabel(new ImageIcon(new ImageIcon("src/rs/raf/gerumap/assets/linus.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+        JLabel jLabelIme2 = new JLabel("Ime: Mihailo");
+        JLabel jLabelPrezime2 = new JLabel("Prezime: Djordjevic");
+        JLabel jLabelIndeks2 = new JLabel("Indeks: 30/20/RN");
+        JLabel jLabelSlika2 = new JLabel("Slika:");
 
-        jPanel.add(jLabelIme); jPanel.add(jLabelImeContent);
-        jPanel.add(jLabelPrezime); jPanel.add(jLabelPrezimeContent);
-        jPanel.add(jLabelIndeks); jPanel.add(jLabelIndeksContent);
-        jPanel.add(jLabelSlika); jPanel.add(jLabelSlikaContent);
+        JLabel jLabelSlikaContent = new JLabel(new ImageIcon(new ImageIcon("src/rs/raf/gerumap/assets/dev1.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+        JLabel jLabelSlikaContent2 = new JLabel(new ImageIcon(new ImageIcon("src/rs/raf/gerumap/assets/dev2.png").getImage().getScaledInstance(-1, 100, Image.SCALE_DEFAULT)));
+
+
+        jPanel.add(jLabelIme); jPanel.add(jLabelIme2);
+        jPanel.add(jLabelPrezime); jPanel.add(jLabelPrezime2);
+        jPanel.add(jLabelIndeks); jPanel.add(jLabelIndeks2);
+        jPanel.add(jLabelSlikaContent); jPanel.add(jLabelSlikaContent2);
+
         jDialog.add(jPanel);
-        jDialog.setVisible(true);
     }
 }
